f0f5d3cb1596bd76d179e884cb4974a41c054a51
30188299 <30188299@nescol.ac.uk>
2023-08-30 14:51:29 +0100

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * *My First Program
+ * @author Stefano Mazzuka
+ * @version 1
+ */
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        // Write out Hello World to the console
+        System.out.println("HelloWorld");
+    }
+
+}

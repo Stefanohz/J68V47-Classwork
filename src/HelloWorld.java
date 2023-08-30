1da8d84d34fdb8878c795b6c5df7bbbadb57234f
30188299 <30188299@nescol.ac.uk>
2023-08-30 14:57:36 +0100

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
     public static void main (String[] args) {
         // Write out Hello World to the console
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
     }
 
 }

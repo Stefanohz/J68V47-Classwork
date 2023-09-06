76b6f9773099a5ffe753cc72f4f034d6352ea824
30188299 <30188299@nescol.ac.uk>
2023-09-06 16:00:12 +0100

    Lesson 2

--- src/L2E1.java
+++ src/L2E1.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class L2E1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your first name? ");
+        String firstName = input.nextLine();
+
+        System.out.print("What is your hobby? ");
+        String hobby = input.nextLine();
+
+        System.out.println("Hello " + firstName + ", glad you like " + hobby);
 
     }
 }
--- src/L2E2.java
+++ src/L2E2.java
@@ -2,9 +2,17 @@ import java.util.*;
 public class L2E2 {
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Item1");
-        String "Item1" = input.nextLine();
+        System.out.println("Enter the first number: ");
+        int num1 = input.nextInt();
 
+        System.out.println("Enter the second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+        System.out.println(num1 + " ^ " + num2 + " = " + (num1^num2));
 
 }
 }

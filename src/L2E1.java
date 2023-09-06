83092742ede8c2fedb40384554cdab8ba6f76e1b
30188299 <30188299@nescol.ac.uk>
2023-09-06 15:52:18 +0100

    Lesson 2

---
+++ src/L2E1.java
@@ -0,0 +1,5 @@
+public class L2E1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/L2E2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class L2E2 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Item1");
+        String "Item1" = input.nextLine();
+
+
+}
+}

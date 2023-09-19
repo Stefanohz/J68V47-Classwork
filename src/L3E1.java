c98d184b023a73f3fc922d5c43147de9181be42f
Stefanohz <30188299@nescol.ac.uk>
2023-09-19 21:31:51 +0100

    L1ProgramingExercises2

--- src/L3E1.java
+++ src/L3E1.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class L3E1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your first name: ");
-        String firstname = "Stefano";
+        System.out.print("Enter your first name:");
+        String firstname = input.nextLine();
         System.out.print(firstname );
 
         System.out.print("Enter your surname: ");
         String surname = input.nextLine();
 
         System.out.print("Enter your year of birth YYYY: ");
-        String yearofbirth = input.nextLine();
+        String year = input.nextLine();
 
+        String Username = firstname.substring(0, 1).toUpperCase() + surname.toLowerCase();
+        String Password = surname.substring(0, 1).toLowerCase() + firstname.substring(0, Math.min(3, firstname.length())).toUpperCase() + year;
 
-        System.out.print("Mazzuka " + surname);
-        System.out.print("1996 " + yearofbirth );
-
-
+        System.out.println("Username: " + Username);
+        System.out.println("Password: " + Password);
 
     }
 }

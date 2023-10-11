2e96b0c73eafad44efd9acdfb44bb7faadd3018e
30188299 <30188299@nescol.ac.uk>
2023-10-11 15:51:02 +0100

    Lesson

---
+++ src/L7Example.java
@@ -0,0 +1,12 @@
+public class L7Example {
+    public static void printMessage(String message){
+        System.out.println(message); // display “Hello world”
+}
+    public static void main(String[] args) {
+        printMessage("Hello world"); // call the printMessage subroutine
+        printMessage("hi b"); // call the printMessage subroutine
+        printMessage("hi c"); // call the printMessage subroutine
+        printMessage("hi d"); // call the printMessage subroutine
+    }
+
+}
